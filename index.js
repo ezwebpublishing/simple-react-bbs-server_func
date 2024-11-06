9f724392ba5b01ab692c94e0fb70197f61751a84
ezwebpub <ezwebpub@gmail.com>
2024-11-06 22:54:59 +0900

    연결정보 수동 입력

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ const upload = multer({ storage: storage })
 
 const mysql = require('mysql');
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: 'mysql-2yt6',
+  user: 'react_bbs',
+  password: '12345',
+  database: 'react_bbs'
 });
 
 db.connect();
