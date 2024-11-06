31c5392724a16c4c467879e3be7efc7377ffd49b
ezwebpub <ezwebpub@gmail.com>
2024-11-06 22:22:08 +0900

    mysql 연결정보 수정

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const port = 8000
-const multer  = require('multer')
+const multer = require('multer')
 
 
 //본문을 통해서 넘어온 요청 파싱(변환) 미들웨어(body-parser)
@@ -32,15 +32,15 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 
-const mysql = require('mysql')
+const mysql = require('mysql');
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'react_bbs',
-  password: '12345',
-  database: 'react_bbs'
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-db.connect()
+db.connect();
 
 /*
 app.get('/', (req, res) => {
@@ -56,56 +56,56 @@ app.get('/', (req, res) => {
 app.get('/list', (req, res) => {
   const sql = "SELECT BOARD_ID, BOARD_TITLE, REGISTER_ID, DATE_FORMAT(REGISTER_DATE , '%Y-%m-%d') AS REGISTER_DATE FROM board";
   db.query(sql, (err, result) => {
-    if (err) throw err;  
+    if (err) throw err;
     res.send(result);
-  })  
+  })
 })
 
 app.get('/detail', (req, res) => {
-  const id = req.query.id; 
+  const id = req.query.id;
   console.log(id);
   const sql = "SELECT BOARD_TITLE, BOARD_CONTENT, IMAGE_PATH FROM board WHERE BOARD_ID = ?";
-  db.query(sql, [id],(err, result) => {
-    if (err) throw err;  
+  db.query(sql, [id], (err, result) => {
+    if (err) throw err;
     res.send(result);
-  })  
+  })
 })
 
-app.post('/insert',upload.single('image'), (req, res) => {
+app.post('/insert', upload.single('image'), (req, res) => {
 
   let title = req.body.title;
   let content = req.body.content;
-  let imagePath = req.file? req.file.path : null;
+  let imagePath = req.file ? req.file.path : null;
 
   const sql = "INSERT INTO board (BOARD_TITLE, BOARD_CONTENT, IMAGE_PATH, REGISTER_ID) VALUES (?,?,?,'admin')";
   db.query(sql, [title, content, imagePath], (err, result) => {
-    if (err) throw err;  
+    if (err) throw err;
     res.send(result);
-  })  
+  })
 })
 
 app.post('/update', (req, res) => {
   // let title = req.body.title;
   // let content = req.body.content;
   // let id = req.body.id;
-  const {id, title, content} = req.body;
+  const { id, title, content } = req.body;
 
   const sql = "UPDATE board SET BOARD_TITLE=?, BOARD_CONTENT=? WHERE BOARD_ID=?";
   db.query(sql, [title, content, id], (err, result) => {
-    if (err) throw err;  
+    if (err) throw err;
     res.send(result);
-  })  
+  })
 })
 
 app.post('/delete', (req, res) => {
   //const boardIDList = req.body.boardIDList;
-  const {boardIDList} = req.body;
+  const { boardIDList } = req.body;
 
   const sql = `DELETE FROM board WHERE BOARD_ID in (${boardIDList})`;
   db.query(sql, (err, result) => {
-    if (err) throw err;  
+    if (err) throw err;
     res.send(result);
-  })  
+  })
 })
 
 
